11397d622940026fa7dd6286dda5c59db6147d33
adrian.bilici <adrian.bilici@student.howest.be>
2026-05-22 15:23:14 +0200

    Flask frontend with Docker Compose Setup

--- api/prestart.sh
+++ api/prestart.sh
@@ -1,5 +1,10 @@
 #! /usr/bin/env sh
+while ! nc -z "$POSTGRES_HOST" "$POSTGRES_PORT"; do
+  sleep 10
+  printf "Database %s:%s not ready" "$POSTGRES_HOST" "$POSTGRES_PORT"
+done
 
+sleep 10
 alembic upgrade head
 
 exec "$@"
